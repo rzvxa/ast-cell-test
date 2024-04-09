38730c6ff0131c688fe41cbe907d7618177fe280
rzvxa <rzvxa@protonmail.com>
2024-04-09 20:53:09 +0330

    add the `unsafe` feature for switching between safe and unsafe implementations.

--- src/ast.rs
+++ src/ast.rs
@@ -1,9 +1,8 @@
 #![allow(dead_code, clippy::enum_variant_names)]
+#[cfg(feature = "unsafe")]
+use std::fmt::Formatter;
 
-use std::{
-    fmt::{Debug, Formatter},
-    marker::PhantomData,
-};
+use std::{fmt::Debug, marker::PhantomData};
 
 #[derive(Default, Debug, Clone, Copy)]
 pub struct NodeId<'a>(usize, PhantomData<&'a ()>);
@@ -20,21 +19,30 @@ impl<'a> NodeId<'a> {
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub enum Statement<'a> {
     ExpressionStatement(NodeId<'a>),
 }
 
 impl<'a> AsAstRef<'a> for Statement<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Stmt,
-            val: AstUntyped { stmt: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Stmt(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Stmt,
+                val: AstUntyped { stmt: self },
+            }
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub enum Expression<'a> {
     StringLiteral(NodeId<'a>),
     Identifier(NodeId<'a>),
@@ -44,9 +52,18 @@ pub enum Expression<'a> {
 
 impl<'a> AsAstRef<'a> for Expression<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Expr,
-            val: AstUntyped { expr: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Expr(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Expr,
+                val: AstUntyped { expr: self },
+            }
         }
     }
 }
@@ -59,14 +76,23 @@ pub struct IdentifierReference<'a> {
 
 impl<'a> AsAstRef<'a> for IdentifierReference<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Ident,
-            val: AstUntyped { ident: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Ident(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Ident,
+                val: AstUntyped { ident: self },
+            }
         }
     }
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct StringLiteral<'a> {
     pub value: &'a str,
     pub parent: NodeId<'a>,
@@ -74,9 +100,18 @@ pub struct StringLiteral<'a> {
 
 impl<'a> AsAstRef<'a> for StringLiteral<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Str,
-            val: AstUntyped { str: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Str(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Str,
+                val: AstUntyped { str: self },
+            }
         }
     }
 }
@@ -91,9 +126,18 @@ pub struct BinaryExpression<'a> {
 
 impl<'a> AsAstRef<'a> for BinaryExpression<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Binary,
-            val: AstUntyped { binary: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Binary(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Binary,
+                val: AstUntyped { binary: self },
+            }
         }
     }
 }
@@ -104,7 +148,7 @@ pub enum BinaryOperator {
     StrictEquality,
 }
 
-#[derive(Clone)]
+#[derive(Debug, Clone)]
 pub struct UnaryExpression<'a> {
     pub operator: UnaryOperator,
     pub argument: NodeId<'a>,
@@ -113,14 +157,23 @@ pub struct UnaryExpression<'a> {
 
 impl<'a> AsAstRef<'a> for UnaryExpression<'a> {
     fn as_ast_ref(&'a mut self) -> AstRef<'a> {
-        AstRef {
-            ty: AstType::Unary,
-            val: AstUntyped { unary: self },
+        #[cfg(not(feature = "unsafe"))]
+        {
+            AstRef {
+                inner: AstKind::Unary(self),
+            }
+        }
+        #[cfg(feature = "unsafe")]
+        {
+            AstRef {
+                ty: AstType::Unary,
+                val: AstUntyped { unary: self },
+            }
         }
     }
 }
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Debug, Clone, Copy, PartialEq)]
 pub enum UnaryOperator {
     UnaryNegation,
     UnaryPlus,
@@ -142,12 +195,215 @@ pub enum AstType {
     Unary,
 }
 
+#[repr(u8)]
 #[derive(Debug)]
+#[cfg(not(feature = "unsafe"))]
+pub enum AstKind<'a> {
+    Stmt(&'a mut Statement<'a>),
+    Expr(&'a mut Expression<'a>),
+    Ident(&'a mut IdentifierReference<'a>),
+    Str(&'a mut StringLiteral<'a>),
+    Binary(&'a mut BinaryExpression<'a>),
+    Unary(&'a mut UnaryExpression<'a>),
+}
+
+#[cfg(not(feature = "unsafe"))]
+impl<'a> Into<AstType> for &AstKind<'a> {
+    fn into(self) -> AstType {
+        match self {
+            AstKind::Stmt(_) => AstType::Stmt,
+            AstKind::Expr(_) => AstType::Expr,
+            AstKind::Ident(_) => AstType::Ident,
+            AstKind::Str(_) => AstType::Str,
+            AstKind::Binary(_) => AstType::Binary,
+            AstKind::Unary(_) => AstType::Unary,
+        }
+    }
+}
+
+#[derive(Debug)]
+#[cfg(not(feature = "unsafe"))]
+pub struct AstRef<'a> {
+    inner: AstKind<'a>,
+}
+
+#[cfg(not(feature = "unsafe"))]
+macro_rules! as_or_panic {
+    ($in:expr, $out:path) => {
+        match $in {
+            $out(out) => out,
+            _ => panic!(),
+        }
+    };
+}
+
+#[cfg(not(feature = "unsafe"))]
+impl<'a> AstRef<'a> {
+    pub fn ast_type(&self) -> AstType {
+        (&self.inner).into()
+    }
+
+    #[inline]
+    pub fn is(&self, ty: AstType) -> bool {
+        self.ast_type() == ty
+    }
+
+    pub fn as_stmt(&self) -> Option<&Statement<'a>> {
+        if self.is(AstType::Stmt) {
+            Some(self.as_stmt_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_expr(&self) -> Option<&Expression<'a>> {
+        if self.is(AstType::Expr) {
+            Some(self.as_expr_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_ident(&self) -> Option<&IdentifierReference<'a>> {
+        if self.is(AstType::Ident) {
+            Some(self.as_ident_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_str(&self) -> Option<&StringLiteral<'a>> {
+        if self.is(AstType::Str) {
+            Some(self.as_str_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_binary(&self) -> Option<&BinaryExpression<'a>> {
+        if self.is(AstType::Binary) {
+            Some(self.as_binary_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_unary(&self) -> Option<&UnaryExpression<'a>> {
+        if self.is(AstType::Unary) {
+            Some(self.as_unary_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_stmt_mut(&mut self) -> Option<&mut Statement<'a>> {
+        if self.is(AstType::Stmt) {
+            Some(self.as_stmt_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_expr_mut(&mut self) -> Option<&mut Expression<'a>> {
+        if self.is(AstType::Expr) {
+            Some(self.as_expr_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_ident_mut(&mut self) -> Option<&mut IdentifierReference<'a>> {
+        if self.is(AstType::Ident) {
+            Some(self.as_ident_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_str_mut(&mut self) -> Option<&mut StringLiteral<'a>> {
+        if self.is(AstType::Str) {
+            Some(self.as_str_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_binary_mut(&mut self) -> Option<&mut BinaryExpression<'a>> {
+        if self.is(AstType::Binary) {
+            Some(self.as_binary_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    pub fn as_unary_mut(&mut self) -> Option<&mut UnaryExpression<'a>> {
+        if self.is(AstType::Unary) {
+            Some(self.as_unary_mut_unchecked())
+        } else {
+            None
+        }
+    }
+
+    // --------- unchecked ---------
+
+    pub fn as_stmt_unchecked(&self) -> &Statement<'a> {
+        as_or_panic!(&self.inner, AstKind::Stmt)
+    }
+
+    pub fn as_expr_unchecked(&self) -> &Expression<'a> {
+        as_or_panic!(&self.inner, AstKind::Expr)
+    }
+
+    pub fn as_ident_unchecked(&self) -> &IdentifierReference<'a> {
+        as_or_panic!(&self.inner, AstKind::Ident)
+    }
+
+    pub fn as_str_unchecked(&self) -> &StringLiteral<'a> {
+        as_or_panic!(&self.inner, AstKind::Str)
+    }
+
+    pub fn as_binary_unchecked(&self) -> &BinaryExpression<'a> {
+        as_or_panic!(&self.inner, AstKind::Binary)
+    }
+
+    pub fn as_unary_unchecked(&self) -> &UnaryExpression<'a> {
+        as_or_panic!(&self.inner, AstKind::Unary)
+    }
+
+    // --------- unchecked mut ---------
+
+    pub fn as_stmt_mut_unchecked(&mut self) -> &mut Statement<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Stmt)
+    }
+
+    pub fn as_expr_mut_unchecked(&mut self) -> &mut Expression<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Expr)
+    }
+
+    pub fn as_ident_mut_unchecked(&mut self) -> &mut IdentifierReference<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Ident)
+    }
+    pub fn as_str_mut_unchecked(&mut self) -> &mut StringLiteral<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Str)
+    }
+
+    pub fn as_binary_mut_unchecked(&mut self) -> &mut BinaryExpression<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Binary)
+    }
+
+    pub fn as_unary_mut_unchecked(&mut self) -> &mut UnaryExpression<'a> {
+        as_or_panic!(&mut self.inner, AstKind::Unary)
+    }
+}
+
+#[derive(Debug)]
+#[cfg(feature = "unsafe")]
 pub struct AstRef<'a> {
     ty: AstType,
     val: AstUntyped<'a>,
 }
 
+#[cfg(feature = "unsafe")]
 impl<'a> AstRef<'a> {
     pub fn ast_type(&self) -> AstType {
         self.ty
@@ -305,6 +561,7 @@ impl<'a> AstRef<'a> {
     }
 }
 
+#[cfg(feature = "unsafe")]
 union AstUntyped<'a> {
     stmt: &'a mut Statement<'a>,
     expr: &'a mut Expression<'a>,
@@ -314,12 +571,14 @@ union AstUntyped<'a> {
     unary: &'a mut UnaryExpression<'a>,
 }
 
+#[cfg(feature = "unsafe")]
 impl<'a> Debug for AstUntyped<'a> {
     fn fmt(&self, _: &mut Formatter<'_>) -> core::fmt::Result {
         Ok(())
     }
 }
 
+#[cfg(feature = "unsafe")]
 impl<'a> AstUntyped<'a> {
     pub fn as_stmt_unchecked(&self) -> &Statement<'a> {
         unsafe { self.stmt }
